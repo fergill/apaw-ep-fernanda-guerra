08269ca0f9b0d69b0e4a38fc1e2b89e6de6e3188
Fernanda <guerragil.fernanda@gmail.com>
2019-10-15 19:44:14 +0200

    Endpoint creation: POST /operators request:{id, name, surname}

---
+++ src/main/java/es/upm/miw/apaw_ep_fernanda_guerra/operator_resource/Operator.java
@@ -0,0 +1,50 @@
+package es.upm.miw.apaw_ep_fernanda_guerra.operator_resource;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Operator {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private String surname;
+
+    private String dni;
+
+    public Operator(String name, String surname, String dni) {
+        this.name = name;
+        this.surname = surname;
+        this.dni = dni;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    @Override
+    public String toString() {
+        return "Operator{" +
+                "id='" + id + '\'' +
+                ", name=" + name +
+                ", surname=" + surname +
+                ", dni=" + dni + '\'' +
+                '}';
+    }
+
+}
