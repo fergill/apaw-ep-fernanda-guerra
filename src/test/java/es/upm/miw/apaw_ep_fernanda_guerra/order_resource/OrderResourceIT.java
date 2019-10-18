a4f1375b5fb1d0a5f99868c21287eb02109ebf38
Fernanda <guerragil.fernanda@gmail.com>
2019-10-18 20:49:19 +0200

    Fixing testReadOperator()

--- src/test/java/es/upm/miw/apaw_ep_fernanda_guerra/order_resource/OrderResourceIT.java
+++ src/test/java/es/upm/miw/apaw_ep_fernanda_guerra/order_resource/OrderResourceIT.java
@@ -1,11 +1,8 @@
 package es.upm.miw.apaw_ep_fernanda_guerra.order_resource;
 
 import es.upm.miw.apaw_ep_fernanda_guerra.ApiTestConfig;
-import es.upm.miw.apaw_ep_fernanda_guerra.croqueta_data.Croqueta;
 import es.upm.miw.apaw_ep_fernanda_guerra.croqueta_resource.CroquetaBasicDto;
-import es.upm.miw.apaw_ep_fernanda_guerra.croqueta_resource.CroquetaCreationDto;
 import es.upm.miw.apaw_ep_fernanda_guerra.croqueta_resource.CroquetaResource;
-import es.upm.miw.apaw_ep_fernanda_guerra.operator_data.Operator;
 import es.upm.miw.apaw_ep_fernanda_guerra.operator_resource.OperatorDto;
 import es.upm.miw.apaw_ep_fernanda_guerra.operator_resource.OperatorResource;
 import org.junit.jupiter.api.Test;
@@ -13,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ApiTestConfig
 public class OrderResourceIT {
@@ -51,19 +48,25 @@ public class OrderResourceIT {
 
     @Test
     void testCreate() {
-            this.createOrder();
+        this.createOrder();
     }
 
     @Test
     void testReadOperator() {
-        Operator operator = new Operator("Aida", "Gil", "1X");
-        Croqueta croqueta = new Croqueta();
-        Order order = new Order(30.00, operator, croqueta);
+        OperatorDto operatorDto = new OperatorDto("Fernanda", "Guerra", "909090X");
+        String operatorId = this.webTestClient
+                .post().uri(OperatorResource.OPERATORS)
+                .body(BodyInserters.fromObject(operatorDto))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(OperatorDto.class)
+                .returnResult().getResponseBody().getId();
+        OrderCreationDto order = new OrderCreationDto(10.00, operatorDto.getId(), "croquetaId");
         this.webTestClient
                 .get().uri(OrderResource.ORDERS + OrderResource.ID_ID + OrderResource.OPERATOR, order)
                 .exchange()
-                .expectBody(Order.class)
-                .returnResult().getResponseBody().getOperator().getName();
-        assertEquals("Aida", order.getOperator().getName());
+                .expectBody(OperatorDto.class)
+                .returnResult().getResponseBody().getId();
+        assertEquals(operatorId, OrderResource.OPERATOR);
     }
 }
