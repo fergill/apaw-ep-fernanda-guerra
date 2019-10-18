e7dcc35ead5af924fa98a87d1dec7ca2ebb9687c
Fernanda <guerragil.fernanda@gmail.com>
2019-10-18 19:58:56 +0200

    Added testReadOperator()

--- src/test/java/es/upm/miw/apaw_ep_fernanda_guerra/order_resource/OrderResourceIT.java
+++ src/test/java/es/upm/miw/apaw_ep_fernanda_guerra/order_resource/OrderResourceIT.java
@@ -1,14 +1,19 @@
 package es.upm.miw.apaw_ep_fernanda_guerra.order_resource;
 
 import es.upm.miw.apaw_ep_fernanda_guerra.ApiTestConfig;
+import es.upm.miw.apaw_ep_fernanda_guerra.croqueta_data.Croqueta;
 import es.upm.miw.apaw_ep_fernanda_guerra.croqueta_resource.CroquetaBasicDto;
+import es.upm.miw.apaw_ep_fernanda_guerra.croqueta_resource.CroquetaCreationDto;
 import es.upm.miw.apaw_ep_fernanda_guerra.croqueta_resource.CroquetaResource;
+import es.upm.miw.apaw_ep_fernanda_guerra.operator_data.Operator;
 import es.upm.miw.apaw_ep_fernanda_guerra.operator_resource.OperatorDto;
 import es.upm.miw.apaw_ep_fernanda_guerra.operator_resource.OperatorResource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ApiTestConfig
 public class OrderResourceIT {
@@ -47,5 +52,18 @@ public class OrderResourceIT {
     @Test
     void testCreate() {
             this.createOrder();
+    }
+
+    @Test
+    void testReadOperator() {
+        Operator operator = new Operator("Aida", "Gil", "1X");
+        Croqueta croqueta = new Croqueta();
+        Order order = new Order(30.00, operator, croqueta);
+        this.webTestClient
+                .get().uri(OrderResource.ORDERS + OrderResource.ID_ID + OrderResource.OPERATOR, order)
+                .exchange()
+                .expectBody(Order.class)
+                .returnResult().getResponseBody().getOperator().getName();
+        assertEquals("Aida", order.getOperator().getName());
     }
 }
